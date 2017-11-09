8bb3143627f9423efc68b749797624ba1bb6bad8
wayne <wzdduncan@163.com>
2017-11-09 22:45:19 +0800

    add:eureka支持

--- src/main/java/com/example/demo/configuration/SpringMvcConfiguration.java
+++ src/main/java/com/example/demo/configuration/SpringMvcConfiguration.java
@@ -3,10 +3,10 @@ package com.example.demo.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-public class SpringMvcConfiguration extends WebMvcConfigurerAdapter {
+public class SpringMvcConfiguration extends WebMvcConfigurationSupport {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
